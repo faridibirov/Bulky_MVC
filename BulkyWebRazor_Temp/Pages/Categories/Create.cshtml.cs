04fb08ac2c1402caf289aba139bd1e2f2b2780a5
Farid Dibirov <farid962@gmail.com>
2024-01-29 03:31:17 +0400

    update and delete functions added to razor project

--- BulkyWebRazor_Temp/Pages/Categories/Create.cshtml.cs
+++ BulkyWebRazor_Temp/Pages/Categories/Create.cshtml.cs
@@ -8,6 +8,7 @@ namespace BulkyWebRazor_Temp.Pages.Categories
     public class CreateModel : PageModel
     {
         private readonly ApplicationDbContext _db;
+        [BindProperty]
         public Category Category { get; set; }
         public CreateModel(ApplicationDbContext db)
         {
@@ -17,5 +18,15 @@ namespace BulkyWebRazor_Temp.Pages.Categories
         public void OnGet()
         {
         }
+
+        public IActionResult OnPost()
+        {
+            _db.Categories.Add(Category);
+            _db.SaveChanges();
+            TempData["success"] = "Category created successfully";
+            return RedirectToPage("Index");
+        }
+
+
     }
 }
