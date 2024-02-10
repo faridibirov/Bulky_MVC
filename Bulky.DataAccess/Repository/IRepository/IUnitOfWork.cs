69dd9c0bacdf9f8ee65dd554bfd379c0d97af838
Farid Dibirov <farid962@gmail.com>
2024-02-11 02:55:35 +0400

    orderheader, orderdetail models added

--- Bulky.DataAccess/Repository/IRepository/IUnitOfWork.cs
+++ Bulky.DataAccess/Repository/IRepository/IUnitOfWork.cs
@@ -13,6 +13,8 @@ public interface IUnitOfWork
 	ICompanyRepository Company { get; }
 	IShoppingCartRepository ShoppingCart { get; }
 	IApplicationUserRepository ApplicationUser { get; }
+	IOrderDetailRepository OrderDetail { get; }
+	IOrderHeaderRepository OrderHeader { get; }
 
 	void Save();
 }
