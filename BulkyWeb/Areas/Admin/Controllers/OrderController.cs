047a8443e49674d0c71f750febb3bccdc6279779
Farid Dibirov <farid962@gmail.com>
2024-02-16 02:03:07 +0400

    order processiing buttons logic

--- BulkyWeb/Areas/Admin/Controllers/OrderController.cs
+++ BulkyWeb/Areas/Admin/Controllers/OrderController.cs
@@ -5,6 +5,7 @@ using Bulky.Utility;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc;
+using System.Security.Claims;
 
 namespace BulkyWeb.Areas.Admin.Controllers;
 
@@ -71,7 +72,23 @@ public class OrderController : Controller
     [HttpGet]
 	public IActionResult GetAll(string status)
 	{
-		IEnumerable<OrderHeader> objOrderHeaders= _unitOfWork.OrderHeader.GetAll(includeProperties: "ApplicationUser").ToList();
+
+		IEnumerable<OrderHeader> objOrderHeaders;
+
+		if(User.IsInRole(SD.Role_Admin) || User.IsInRole(SD.Role_Employee))
+		{
+			objOrderHeaders = _unitOfWork.OrderHeader.GetAll(includeProperties: "ApplicationUser").ToList();
+        }
+		else
+		{
+            var claimsIdentity = (ClaimsIdentity)User.Identity;
+            var userId = claimsIdentity.FindFirst(ClaimTypes.NameIdentifier).Value;
+
+			objOrderHeaders = _unitOfWork.OrderHeader
+				.GetAll(u => u.ApplicationUserId == userId, includeProperties: "ApplicationUser");
+
+        }
+
 
 		switch (status)
 		{
