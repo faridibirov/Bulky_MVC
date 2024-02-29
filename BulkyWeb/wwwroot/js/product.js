978ffcce03acaac9458069d31a8a7bad18f88c84
Farid Dibirov <farid962@gmail.com>
2024-02-29 18:29:40 +0400

    localization for datatable

--- BulkyWeb/wwwroot/js/product.js
+++ BulkyWeb/wwwroot/js/product.js
@@ -1,12 +1,45 @@
 ﻿var dataTable;
+
+var languageStrings = {
+    'en': {
+        'languageUrl': "//cdn.datatables.net/plug-ins/9dcbecd42ad/i18n/English.json",
+        'deleteConfirmationTitle': 'Are you sure?',
+        'deleteConfirmationText': 'You won\'t be able to revert this!',
+        'deleteConfirmationButtonText': 'Yes, delete it!',
+        'editButtonText': 'Edit',
+        'deleteButtonText': 'Delete',
+        deleteCancelButtonText: 'Cancel',
+        'successMessage': 'Operation successful!'
+    },
+    'ru': {
+        'languageUrl': "//cdn.datatables.net/plug-ins/9dcbecd42ad/i18n/Russian.json",
+        'deleteConfirmationTitle': 'Вы уверены?',
+        'deleteConfirmationText': 'Вы не сможете отменить это действие!',
+        'deleteConfirmationButtonText': 'Да, удалить!',
+        'editButtonText': 'Редактировать',
+        'deleteButtonText': 'Удалить',
+        deleteCancelButtonText: 'Отмена',
+        'successMessage': 'Операция выполнена успешно!'
+    }
+};
+
+
 $(document).ready(function () {
     loadDataTable();
 });
-
+    
 function loadDataTable() {
+    var languageUrl = languageStrings[cultureInfo].languageUrl;
+
+    if ($.fn.DataTable.isDataTable('#tblData')) {
+        $('#tblData').DataTable().destroy();
+    }
+
     dataTable = $('#tblData').DataTable({
         "ajax": { url: '/admin/product/getall' },
-
+        "language": {
+            "url": languageUrl
+        },
         "columns": [
             { data: 'title', "width":"25%" },
             { data: 'isbn', "width": "15%" },
@@ -17,8 +50,8 @@ function loadDataTable() {
                 data: 'id',
                 "render": function (data) {
                     return `<div class="w-75 btn-group" role="group">
-                     <a href="/admin/product/upsert?id=${data}" class="btn btn-primary mx-2"> <i class="bi bi-pencil-square"></i> Edit</a>               
-                     <a onClick=Delete('/admin/product/delete/${data}') class="btn btn-danger mx-2"> <i class="bi bi-trash-fill"></i> Delete</a>
+                     <a href="/admin/product/upsert?id=${data}" class="btn btn-primary mx-2"> <i class="bi bi-pencil-square"></i> ${languageStrings[cultureInfo].editButtonText}</a>               
+                     <a onClick=Delete('/admin/product/delete/${data}') class="btn btn-danger mx-2"> <i class="bi bi-trash-fill"></i> ${languageStrings[cultureInfo].deleteButtonText}</a>
                     </div>`
                 },
                 "width": "25%"
@@ -29,13 +62,14 @@ function loadDataTable() {
 
 function Delete(url) {
     Swal.fire({
-        title: "Are you sure?",
-        text: "You won't be able to revert this!",
+        title: languageStrings[cultureInfo].deleteConfirmationTitle,
+        text: languageStrings[cultureInfo].deleteConfirmationText,
         icon: "warning",
         showCancelButton: true,
         confirmButtonColor: "#3085d6",
         cancelButtonColor: "#d33",
-        confirmButtonText: "Yes, delete it!"
+        confirmButtonText: languageStrings[cultureInfo].deleteConfirmationButtonText,
+        cancelButtonText: languageStrings[cultureInfo].deleteCancelButtonText
     }).then((result) => {
         if (result.isConfirmed) {
             $.ajax({
